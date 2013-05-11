330e2ea5d1f48bd8871740efa1fd1c380decf16b
tahsin.tanzim@gmail.com <tahsin.tanzim@gmail.com>
2013-05-11 16:12:30 -0400

    commit[2]

---
+++ headers/DEBUG.h
@@ -0,0 +1,29 @@
+#ifndef __DEBUG_H__
+#define __DEBUG_H__
+
+#include <iostream>
+#include <string>
+
+template <class ty>
+void DEBUG( std::string s1, ty t1 )
+{
+    std::cout << "DEBUGGING from " << s1
+	    << " :: " << t1 << std::endl;
+}
+template <class ty>
+void DEBUG( std::string s1, ty t1, ty t2 )
+{
+    std::cout << "DEBUGGING from " << s1
+	    << " :: " << t1 << ", "
+	    << t2 << std::endl;
+}
+
+template <class ty>
+void DEBUG( std::string s1, ty t1, ty t2, ty t3 )
+{
+    std::cout << "DEBUGGING from " << s1
+	    << " :: " << t1 << ", "
+	    << t2 << ", " << t3 << std::endl;
+}
+
+#endif
---
+++ src/angle.cpp
@@ -0,0 +1,13 @@
+#include "../headers/angle.h"
+#include "../headers/Pos.h"
+
+anglePI::anglePI(const anglePI& an) { value = an.value; }
+anglePI::anglePI(const angleRAD& an) { value = an.value / PI; }
+anglePI::anglePI(const angleDEG& an) { value = an.value / 180; }
+
+angleRAD::angleRAD(const angleRAD& an) { value = an.value; }
+angleRAD::angleRAD(const angleDEG& an) { value = an.value * (180.0 / PI); }
+angleRAD::angleRAD(const anglePI& an) { value = an.value * PI; }
+
+angleDEG::angleDEG(const angleDEG& an) { value = an.value; }
+angleDEG::angleDEG(const angleRAD& an) { value = an.value * (PI / 180.0); }
